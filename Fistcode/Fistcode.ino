e59c7a59f67d38b0ff6ca10f62a8010f761b37cc
CFingers <44417257+CFingers@users.noreply.github.com>
2018-11-29 14:52:30 -0800

    Update Fistcode.ino

--- Fistcode/Fistcode.ino
+++ Fistcode/Fistcode.ino
@@ -1,9 +1,11 @@
 #include <Servo.h>
+#include <peacesign.ino>
 
 Servo servo1;
 Servo servo2;
 
 int sP = 0;
+int count = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -17,17 +19,26 @@ void loop() {
   float voltage;
   voltage = getVoltage(EMG)
 
-  Serial.print1n(voltage)
-  if(voltage) >=1.5 & <= 4.5
-  delay(191);
-  servo1.write(127);
-  delay(191);
-  servo2.write(127)
-  delay(191);
+  if(voltage) >=1.5 & <= 4.5 {
+   delay(191);
+   servo1.write(127);
+   delay(191);
+   servo2.write(127)
+   delay(191);
+   count = count+1;
   else
-  delay(191);
-  servo1.write(sP);
-  deay(191)
-  servo2.write(sP);
-
+   delay(191);
+   servo1.write(sP);
+   deay(191)
+   servo2.write(sP);
+   count = count+1;
 }
+ Serial.print1n(voltage);
+ delay(5);
+ Serial.print1n(servo1);
+ delay(5);
+ Serial.print1n(servo2);
+ delay(5);
+ Serial.print1n(count);
+}
+
